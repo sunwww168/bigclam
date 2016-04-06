f2cf2fe84d8aa98337de13e21ecf981a5c18dbe9
leeyifu <yifuleeif@gmail.com>
2016-04-06 20:54:18 +0800

    ER with 4 communities

--- BigClam.py
+++ BigClam.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from itertools import combinations
-from agm import AGM, community
+# from agm import AGM, community
 import numpy as np
 import random
 
@@ -28,31 +28,48 @@ def bigclam(graph, community_num, step_size=0.01,threshold=0.01): # max_iter
 			gradient += u - np.sum(F,axis=0)
 			# update row u
 			F[node,:] = F[node,:] + step_size*gradient
-			# check for non-negative
+			# check for non-negative constrain
 			for i in range(community_num):
 				if F[node,i] < 0:
 					F[node,i] = 0
-		if np.sum(np.multiply(F-last_F,F-last_F)) <= 0.01:
+		print 'iter is ', iter
+		change = np.sum(np.multiply(F-last_F,F-last_F))
+		if  change <= 0.01:
 			break
+		print 'change is ', float(change)
 	print iter
 	return F
 
-nodes = range(24)
-communities = [community(range(12), 0.8),
-               community(range(6,17), 0.8),
-               community(range(8,12)+range(17,24), 0.8)]
-#random.seed(123)
-G = AGM(nodes,communities)
-F = bigclam(G, 3)
+def GenerateGraph():
+	G = nx.erdos_renyi_graph(200, 0.01)
+	# 4 communities 
+	community_1 = range(20)+range(50,80)+range(110,140)
+	community_2 = range(18,68)+range(150,180)
+	community_3 = range(23)+range(76,123)
+	community_4 = range(160,200)+range(30,66)
+	communities = [community_1,community_2,community_3,community_4]
+	for community in communities:
+		random.shuffle(community)
+		for i in range(230):
+			pair = random.sample(community, 2)
+			G.add_edge(pair[0], pair[1])
+	return G
+
+G = GenerateGraph()
+F = bigclam(G, 4)
 print F
 
 
+# nodes = range(24)
+# communities = [community(range(12), 0.8),
+#                community(range(6,17), 0.8),
+#                community(range(8,12)+range(17,24), 0.8)]
+# G = AGM(nodes,communities)
+# F = bigclam(G, 3)
+# print F
+
+
 			
-
-
-
-
-
 
 
 # BigClam's Network Generation Model:
